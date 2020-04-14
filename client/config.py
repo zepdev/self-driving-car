ee4bac4f655a2a214254a4a88eee6c7d6095bee9
nils.sturma <nils.sturma@zeppelin.com>
2020-04-14 14:49:02 +0200

    tested running the server on aws, works fine

--- client/config.py
+++ client/config.py
@@ -1,4 +1,5 @@
-MSG_BROKER_BASE_URL = "http://localhost:80"
+MSG_BROKER_BASE_URL = "http://ec2-18-194-33-91.eu-central-1.compute.amazonaws.com"
+# "http://localhost:80"
 USER = "RCcar-Rpi"
 PASSWORD = "lwjg#?kXTV&SCreh46"
 
