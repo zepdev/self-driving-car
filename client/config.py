ba96346db7411183424def45f58a1c34c35b300f
nils.sturma <nils.sturma@zeppelin.com>
2020-04-14 12:08:03 +0200

    moved recordings directory to client and added it to gitignore

--- client/config.py
+++ client/config.py
@@ -2,5 +2,5 @@ MSG_BROKER_BASE_URL = "http://localhost:80"
 USER = "RCcar-Rpi"
 PASSWORD = "lwjg#?kXTV&SCreh46"
 
-LOCAL_PATH_RECORDINGS = "../recordings"
+LOCAL_PATH_RECORDINGS = "recordings"
 SENDER_SLEEP_TIME = 10
