3742f1b046454820cc42538a81dfe7f13650b7b7
nils.sturma <nils.sturma@zeppelin.com>
2020-05-25 10:15:03 +0200

    client/app is not longer included in docker-compose

--- client/app/config.py
+++ client/app/config.py
@@ -2,11 +2,11 @@ import os
 import logging
 
 # Redis
-REDIS_HOST = os.environ["REDIS_HOST"]
-REDIS_PORT = os.environ["REDIS_PORT"]
-DB_ID = os.environ["DB_ID"]
-QUEUE_NAME = os.environ["QUEUE_NAME"]
+REDIS_HOST = "localhost"
+REDIS_PORT = "6379"
+DB_ID = "0"
+QUEUE_NAME = "queue"
 
 # Logging
-LOG_LEVEL = os.environ["LOG_LEVEL"]
+LOG_LEVEL = "INFO"
 logging.basicConfig(format="%(asctime)s %(message)s", level=LOG_LEVEL)
