90508267079f52ef702e9f915bdd83d69b55a326
nils.sturma <nils.sturma@zeppelin.com>
2020-08-25 17:07:49 +0200

    started writing own class for motor driver hat - still WIP

--- client/app/drive.py
+++ client/app/drive.py
@@ -1,6 +1,49 @@
 import RPi.GPIO as GPIO
-from dual_g2_hpmd_rpi import motors
+from dual_g2_hpmd_rpi import motors  # TODO: write this by myself, see below
 
+
+class Motor():
+
+    def __init__(self):
+        # this is from the dual_g2_hpmd_rpi library
+        self.pwm_pin = 13
+        self.dir_pin = 25
+        self.en_pin = 23
+        self.flt_pin = 6
+
+        self.MAX_SPEED = 480
+
+        # TODO: Translate the following to GPIO: https://sourceforge.net/p/raspberry-gpio-python/wiki/PWM/
+        # (from pigpio: http://abyz.me.uk/rpi/pigpio/python.html)
+
+        #_pi.set_pull_up_down(flt_pin, pigpio.PUD_UP)  # make sure FLT is pulled up
+        #_pi.write(en_pin, 1)  # enable driver by default
+
+    def setSpeed(self, speed):
+        if speed < 0:
+            speed = -speed
+            dir_value = 1
+        else:
+            dir_value = 0
+
+        if speed > self.MAX_SPEED:
+            speed = self.MAX_SPEED
+
+        # TODO: translate this
+        #_pi.write(self.dir_pin, dir_value)
+        #_pi.hardware_PWM(self.pwm_pin, 20000, int(speed * 6250 / 3));
+          # 20 kHz PWM, duty cycle in range 0-1000000 as expected by pigpio
+
+    def enable(self):
+        # TODO: translate this
+        #_pi.write(self.en_pin, 1)
+
+    def disable(self):
+        # TODO: translate this
+        #_pi.write(self.en_pin, 0)
+
+
+# TODO: instantiate motor from Motor (maybe give the pins?) and change code below accordingly
 
 class Drive():
     
