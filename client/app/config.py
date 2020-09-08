6b3e4dd6d0cad2bafb2bc951fa1d48138ef9a50f
NilsSturma <nils.sturma@web.de>
2020-09-08 09:04:02 +0200

    new config

--- client/app/config.py
+++ client/app/config.py
@@ -37,7 +37,7 @@ servo_angles = {
 }
 
 # Others
-START_SLEEP_TIME = 5  # Sleep long on autostart to give docker-compose time to start
+START_SLEEP_TIME = 20  # Sleep long on autostart to give docker-compose time to start
 RECORD_SLEEP_TIME = 0.6
 MAIN_SLEEP_TIME = 1
 
